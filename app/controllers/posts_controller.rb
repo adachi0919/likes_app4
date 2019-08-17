0f8d76befe45f09233061cfcb91917dcc771e237
keisukeadachi <c3a578zx@gmail.com>
2019-08-17 20:53:22 +0900

    add_destroy_and_edit

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -29,6 +29,22 @@ class PostsController < ApplicationController
     end
   end
 
+  def edit
+    @post = Post.find(params[:id])
+  end
+
+  def update
+    post = Post.find(params[:id])
+    post.update(post_params)
+    redirect_to :action => :"index"
+  end
+
+  def destroy
+    post = Post.find(params[:id])
+    post.destroy
+    redirect_to :action => :"index"
+  end
+
   private
   def post_params
     params.require(:post).permit(:content)
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
 
   resources :users, only: [:index, :show]
-  resources :posts, only: [:index, :show, :create] do
+  resources :posts do
     resources :likes, only: [:create, :destroy]
   end
 
